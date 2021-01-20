a0b634b789789d86fab94d685727386b585add9c
Maksim Koltsov <tirinox@gmail.com>
2021-01-20 14:03:56 +0300

    fix crash

--- app/services/notify/types/bridge_health_notify.py
+++ app/services/notify/types/bridge_health_notify.py
@@ -14,15 +14,24 @@ class HealthNotifier(INotified):
 
     KEY_LAST_STATUS = 'bridge_last_status'
 
-    async def on_data(self, sender, data: BridgeHealth):
+    async def get_old_status(self):
         redis = await self.deps.db.get_redis()
         if self.last_status is None:
-            self.last_status = (await redis.get(self.KEY_LAST_STATUS)).decode()
-            if self.last_status is None:
-                self.last_status = BridgeHealth.STATUS_OFFLINE
+            last_status = (await redis.get(self.KEY_LAST_STATUS))
+            if last_status is None:
+                last_status = BridgeHealth.STATUS_OFFLINE
+            else:
+                last_status = last_status.decode()
+            self.last_status = last_status
+        return self.last_status
+
+    async def on_data(self, sender, data: BridgeHealth):
+        self.last_status = await self.get_old_status()
 
         if data.status != self.last_status:
             self.last_status = data.status
+
+            redis = await self.deps.db.get_redis()
             await redis.set(self.KEY_LAST_STATUS, data.status)
 
             users = await self.deps.broadcaster.all_users()
